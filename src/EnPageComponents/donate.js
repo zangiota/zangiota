7f3d08239183b89c143e1b80d03ca2deba28ec80
zangiota <zangiota.ziyoratgoh@mail.ru>
2023-08-15 07:49:51 +0500

    15.08.2023

--- src/EnPageComponents/donate.js
+++ src/EnPageComponents/donate.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import donate from '../images/maruza1.mp4'
-import donate1 from '../images/maruza2.mp4'
+// import donate from '../images/maruza1.mp4'
+// import donate1 from '../images/maruza2.mp4'
 import '../styles/video.css'
 
 class video extends Component {
@@ -10,13 +10,19 @@ class video extends Component {
       <div className='video_content'>
       <h1 className='video_title'>/ About us</h1>
       <h3 className='video_cl'> Last news</h3>
-      <p className='video_text'><span className='text_news'>Religious</span>Religious teaching Shosatova Shakira, religious worker of our memorial complex, about donation and dears:</p>
-      <video className='video_mosque' controls >
+      <span className='text_news'>
+      A GOOD WOMAN IMPROVES THE WORLD</span>
+      <p className='video_text'> 
+      
+       Our devotees say: "A man gains respect because of a righteous woman, and a household prospers because of a wise woman." Even if the owner of the house is a prophet, because of a bad woman, the house will be destroyed.'' In our language, this wise word corresponds to the phrase: "It is the wife who makes the husband a husband, and the wife who makes the husband black." In fact, being smart and wise of a housewife is happiness and bliss in every way. An example of this is father Zangi and mother Anbar. When father Zangi turned 25, he married mother Anbar, the 40-year-old daughter of Khorezmshah, and lived in Tashkent region. This breed thoroughly assimilates the Naqshbandiya and Yassaviya sects. He propagated Islam throughout Central Asia. Zangi's father, whose original profession was a shepherd, was in the service of his friends by taking care of the livestock of his neighbors. Father Zangi was childless and lived to be about 86 years old. His wife Anbar was the daughter of Khorezmshah Bogrokhan (late 12th century). The king gives his daughter in marriage to Solomon father (Hakim father, Zangi father's mentor). They have 3 sons. However, Hakim's father dies at a young age. Before his death, he bequeathed to his sons: "My children, after my death, 40 saints will visit me. Among them will be a tall saint with a staff in his hands. "Marry your mother to him." According to the will of his father, Zangi marries a woman who is fifteen years older than him and has three children. Despite being the daughter of a king, mother Anbar served and received the blessing of saint Zangi father. Mother Anbar herself served as a midwife. She was an example for all women with her purity, chastity, and manners. Mother Anbar and father Zangi died about a year apart at the end of the 13th century. In the example of Mother Anbar, we understand that a righteous woman improves the world. A righteous woman is half of religion. A virtuous woman is the best of the cloths of the world. Narrated by Abdullah ibn Amr, may Allah be pleased with him. Our Prophet, may God bless him and grant him peace, said: "The world is a fabric. The best of the cloths of the world is a virtuous woman" (narrated by Imam Muslim). Imam Hokim narrates from Anas in his "Mustadrak". The Prophet, peace be upon him, said: "Whoever Allah has blessed with a righteous woman, Allah has helped him with half of his religion. In the remaining half, let him fear Allah." Regarding the adornment of a righteous woman, Abu Nu'aym says: "Said ibn Musayyib's wife said, 'We used to say to our husbands, as you say to your emirs, 'May Allah make you strong, may Allah grant you health.'" A kind and loving mother is a source of family happiness and peace, a candle that delights the hearts and eyes of family members with her kindness. In fact, a man whose wife is kind, sweet, and loyal will rush to return home to his family as soon as possible. After studying and training, the children also go to the arms of their loving mothers. A woman is a loyal, loving mother, and the more she pays attention to the care and education of her family members, the more she always draws them to herself like a slave. A household with such a woman is the happiest and most peaceful family. Among us, there are women and girls who run away with the dream of getting a position or gaining prestige or impressing others with their beauty. Unfortunately, they want to find their happiness on the street. In the end, what they thought was happiness turned out to be a mirage. Because they traded their real happiness - their place in the family - for some trivial things. If they devoted themselves to taking good care of their children, raising them to be healthy, smart and polite, they would have brought great benefits not only to themselves, but also to the society and the state. The fact that a person grows up to be honest, self-sacrificing, noble and faithful is closely related to the family environment. A child will never become a necessary person for the society and the nation by itself. For this, parents' love, attention, and education are necessary. The position of mothers in this matter cannot be compared with anything.
+      </p>
+      
+      {/* <video className='video_mosque' controls >
       <source src={donate} type="video/mp4"/>
      </video>
      <video className='video_mosque' controls >
             <source src={donate1} type="video/mp4" />
-          </video>
+          </video> */}
       </div>
       </div>
     );
